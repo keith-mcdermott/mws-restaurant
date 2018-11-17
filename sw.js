13ad617c113cdb7b37823558b4020e2606696273
Keith McDermott <mcdermott.km@gmail.com>
2018-11-16 21:26:01 -0500

    updated sw

--- sw.js
+++ sw.js
@@ -6,31 +6,32 @@ console.log('SW Registered');
 let staticCacheName = 'restaurant-static-v1';
 //files to cache
 let filesToCache = [
-  './',
-  './index.html',
-  './restaurant.html',
-  './css/styles.css',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './js/dbhelper.js',
-  './js/sw_registration.js',
-  './data/restaurants.json',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
+  '/',
+  'index.html',
+  'restaurant.html',
+  'css/styles.css',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'js/dbhelper.js',
+  'js/sw_registration.js',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
 ];
 
 // add to cache
 self.addEventListener('install', function(event) {
 	event.waitUntil(
-		caches.open(staticCacheName).then(function(cache) {
+		caches.open(staticCacheName)
+    .then(function(cache) {
 			return cache.addAll(filesToCache);
         })
     );
@@ -56,7 +57,14 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            return response || fetch(event.request)
+// from https://developers.google.com/web/ilt/pwa/lab-caching-files-with-service-worker
+            .then(response => {
+              return caches.open(staticCacheName).then(cache => {
+                cache.put(event.request.url, response.clone());
+                return response;
+              });
+            });
         })
     );
 });
